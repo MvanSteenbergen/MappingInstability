4a6a14d3c798d72beb35a8bc6a3aa31a9be0b455
MvanSteenbergen <maasvansteenbergen@kolabnow.com>
2023-10-01 16:11:48 +0200

    Made code more readable

--- Early simulation/modpsy.jl
+++ Early simulation/modpsy.jl
@@ -3,16 +3,12 @@ using DataFrames
 using Random
 
 # DATA GENERATION
-function generateData()
-    Smax, Rs, λs, τx, P, Rb, λb, L, τy, S, α, β, τz = 10, 1, 0.1, 14, 10, 1.04, 0.05, 1.01, 14, 10, 0.5, 0.5, 1 
+function generateData(Smax, Rs, λs, τx, P, Rb, λb, L, τy, S, α, β, τz)
 
     Li = collect(1:1200000)
-    dt = 0.01
+    δt = 0.01
     Lt = [dt * i for i in Li]
-    y = 0.01 
-    x = 0.0
-    z = 0.0
-    f = 0.0
+    y, x, z, f = 0.01, 0.0, 0.0, 0.0
     Ly = []
     Lx = []
     Lz = []
@@ -20,19 +16,21 @@ function generateData()
 
     for i in Li
         a = rand() - 0.5
-        dx = (Smax /(1 + exp((Rs - y) / λs)) - x) / τx
-        dy = (P / (1 + exp((Rb - y) / λb)) + L * f - x * y- z) 
-        dz = (S * (α * x + β * y) * a - z) / τz
-        df = (y - 1.0 * f)/720
-        y = y + dy * t 
-        x = x + dx * dt
-        z = z + dz * dt
-        f = f + df * dt
+        δx = (Smax /(1 + exp((Rs - y) / λs)) - x) / τx
+        δy = (P / (1 + exp((Rb - y) / λb)) + L * f - x * y - z) 
+        δz = (S * (α * x + β * y) * a - z) / τz
+        δf = (y - 1.0 * f) / 720
+        y = y + δy * δt 
+        x = x + δx * δt
+        z = z + δz * δt
+        f = f + δf * δt
         push!(Ly, y)
         push!(Lx, x)
         push!(Lz, z)
         push!(Lf, f)
     end
+
+    return Ly, Lx, Lz, Lf
 end
 
-function ()
+data = generateData(10, 1, 0.1, 14, 10, 1.04, 0.05, 1.01, 14, 10, 0.5, 0.5, 1 )
