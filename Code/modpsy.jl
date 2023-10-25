b4eaa8082133cb3c6a698b3cb7a5e121b6b157c6
MvanSteenbergen <mvansteenbergen@proton.me>
2023-10-25 19:03:33 +0200

    added package environment

--- Code/modpsy.jl
+++ Code/modpsy.jl
@@ -5,7 +5,7 @@ using RecurrenceAnalysis
 
 # DATA GENERATION
 function generateData(Smax, Rs, λs, τx, P, Rb, λb, L, τy, S, α, β, τz)
-    Li = collect(1:100000)
+    Li = collect(1:500000)
     δt = 0.01
     Lt = [δt * i for i in Li]
     y, x, z, f = 0.01, 0.0, 0.0, 0.0
@@ -18,7 +18,7 @@ function generateData(Smax, Rs, λs, τx, P, Rb, λb, L, τy, S, α, β, τz)
         a = rand() - 0.5
         
         δx = (Smax /(1 + exp((Rs - y) / λs)) - x) / τx
-        δy = (P / (1 + exp((Rb - y) / λb)) + L * f - x * y - z) 
+        δy = (P / (1 + exp((Rb - y) / λb)) + L * f - x * y - z) / τy
         δz = (S * (α * x + β * y) * a - z) / τz
         δf = (y - 1.0 * f) / 720
 
@@ -33,29 +33,33 @@ function generateData(Smax, Rs, λs, τx, P, Rb, λb, L, τy, S, α, β, τz)
         push!(Lf, f)
     end
 
-    return DataFrame(Ly = Ly, Lx = Lx, Lz = Lz, Lf = Lf, Lt = Lt)
+    return DataFrame(Li = Li, Ly = Ly, Lx = Lx, Lz = Lz, Lf = Lf, Lt = Lt)
 end
 
+
+
 # Timepoint reduction function
-function reduceTimepoints(data, tReduction)
-    index = floor(range(1, nrow(data), tReduction))
-    return data[index]
+function reduceTimepoints(data, reductionFactor)
+    data = filter(:Li => x -> x % reductionFactor == 0, data)
+    data.Li = 1:nrow(data)
+    return data
 end
 
 # Data degredation for experiment
-function degradeData(data, segmentN, tReduction)
+function degradeData(data, segmentN, reductionFactor)
     segmentLength = (maximum(data.Lx) - minimum(data.Lx)) / segmentN
 
     # Take the modulus and remove it from the values
     data.Ax = [data.Lx[i] - mod(data.Lx[i], segmentLength) for i in 1:length(data.Lx)]
 
     # Remove unwanted timepoints and return
-    return reduceTimepoints(data, tReduction)
+    return reduceTimepoints(data, reductionFactor)
 end
 
-# Calculation of recurrence indicators
-function recurrenceStatistics
-    recurrence
-
 data = generateData(10, 1, 0.1, 14, 10, 1.04, 0.05, 1.01, 14, 10, 0.5, 0.5, 1)
+reducedData = reduceTimepoints(data, 100)
+degradedData = degradeData(reducedData, 7, 2)
+
+plot(x = reducedData.Li, y = reducedData.Lx)
+plot(x = degradedData.Li, y = degradedData.Ax)
 
