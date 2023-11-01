eaccdf3dd510b666094377c2eb25f7283d7d3426
MvanSteenbergen <mvansteenbergen@proton.me>
2023-11-01 18:45:12 +0100

    simulation works!

--- Code/simulation_dynamical_systems.jl
+++ Code/simulation_dynamical_systems.jl
@@ -3,10 +3,10 @@ using DataFrames
 using DifferentialEquations
 using Distributions
 using DynamicalSystems
-using Profile
 using Random
 using Statistics
-using Sundials
+
+## DATA GENERATION
 
 """
     threePlusOneDimensions!(du, u, p, t)
@@ -21,12 +21,10 @@ This function implements the 3 + 1 Dimensions Model. Please check the paper for 
     - 't' = : Used by coupledODEs function. Does something under the hood.
 """
 function threePlusOneDimensions!(du, u, p, t)
-    Smax, τx, τy, τz, τf, α, β, P, S, Rᵦ, Rₛ, L, λₛ, ζ = p
-
+    Smax, Rₛ, λₛ, τx, P, Rᵦ, λᵦ, L, τᵧ, S, α, β, τz, ζ = p
     du[1] = (Smax /(1 + exp((Rₛ - u[2]) / λₛ)) - u[1]) / τx
-    du[2] = (P / (1 + exp((Rᵦ - u[2]) / λₛ)) + L - u[1] * u[2] - u[3]) / τy
+    du[2] = (P / (1 + exp((Rᵦ - u[2]) / λᵦ)) + L - u[1] * u[2] - u[3]) / τᵧ
     du[3] = (S * (α * u[1] + β * u[2]) * ζ - u[3]) / τz
-
     return nothing
 end 
 
@@ -42,27 +40,31 @@ end
 
 cb = PeriodicCallback(affect!, 0.01)
 
-# Set the starting conditions and parameters of the model
-p₀ = [10, 14, 14, 1, 1, 0.5, 0.5, 10, 10, 1.04, 1, 1.01, 0.05, 0.1, 0.0]
-u₀ = [0.0, 0.1, 0.0, 0.0]
-
-# Saves the threePlusOneDimensions as a Dynamical Systems object using the rule specified above
-threePlusOneDimensions = ODEProblem(threePlusOneDimensions!, u₀, (0.0,100000.0), p₀)
-
-X = solve(threePlusOneDimensions,  callback=cb, reltol = 1e-9, abstol = 1e-9)
-
-X = DataFrame(X)
-
-
-# Create figure
-fig = Figure()
-ax = Axis(fig[1, 1]; xlabel = "time", ylabel = "variable")
-for var in columns(X)
-    lines!(ax, t, var)
+function calculateTrajectory(p₀)
+    u₀ = [0.0, 0.1, 0.0]
+    prob = ODEProblem(threePlusOneDimensions!, u₀, (0.0,8000.0), p₀)
+    X = solve(prob, callback=cb, reltol = 1e-9, abstol = 1e-9)
+    return X
 end
-fig[1, 2] = Legend(fig, ax, "Variables", framevisible = false)
 
-# Show figure
-fig
+# Set the starting conditions and parameters of the model
+healthy = calculateTrajectory([10, 1, 0.1, 14, 10, 1.04, 0.05, 0.2, 14, 4, 0.5, 0.5, 1, 0.2])
+schizophrenia = calculateTrajectory([10, 1, 0.1, 14, 10, 0.904, 0.05, 0.2, 14, 4, 0.5, 0.5, 1, 0.2])
+bipolar = calculateTrajectory([10, 1, 0.1, 14, 10, 1.04, 0.05, 1.01, 14, 10, 0.5, 0.5, 1, 0.2])
+bereavement = calculateTrajectory([10, 1, 0.1, 14, 10, 1, 0.05, 0.6, 14, 4.5, 0.5, 0.5, 1, 0.2])
+
+## DATA DEGRADATION
+
+# The method takes 
 
 
+
+function degradeData(data, segmentN, reductionFactor)
+    segmentLength = (maximum(data.Lx) - minimum(data.Lx)) / segmentN
+
+    # Take the modulus and remove it from the values
+    data.x = [data.Lx[i] - mod(data.Lx[i], segmentLength) for i in 1:length(data.Lx)]
+
+    # Remove unwanted timepoints and return
+    return reduceTimepoints(data, reductionFactor)
+end
