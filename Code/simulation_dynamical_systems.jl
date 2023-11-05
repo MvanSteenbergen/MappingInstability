2fb582ca5b84f87167922b70846ee48f21cee53a
MvanSteenbergen <mvansteenbergen@proton.me>
2023-11-05 12:53:22 +0100

    Added RQA for each system

--- Code/simulation_dynamical_systems.jl
+++ Code/simulation_dynamical_systems.jl
@@ -4,8 +4,8 @@ using DifferentialEquations
 using Distributions
 using DynamicalSystems
 using Random
+using RecurrenceAnalysis
 using Statistics
-using Sundials
 
 ## DATA GENERATION
 
@@ -17,7 +17,7 @@ This function implements the 3 + 1 Dimensions Model. Please check the paper for 
 
     # Arguments
     - 'du' = : The derivatives of the functions x, y, z, f
-    - 'u' = : The functions x, y, z
+    - 'u' = : The functions x, y, z, f
     - 'p' = : Parameters Smax, Rₛ, λₛ, τx, P, Rᵦ, λᵦ, L, τᵧ, S, α, β, τz, ζ 
     - 't' = : Used by ODEProblem function. Does something under the hood.
 """
@@ -37,60 +37,80 @@ end
 This function adds Gaussian noise between 0 and 1 at each 0.01th timepoint and saves it as the ζ-term (which is equivalent to integrator.p[14]).
 """
 function affect!(integrator)
-    integrator.p[14] = rand(Normal(0, 0.5))
-    if integrator.p[14] > 1
-        integrator.p[14] = 1.0
-    elseif  integrator.p[14] < -1
-        integrator.p[14] = -1.0
-    end
+    integrator.p[14] = clamp(rand(Normal(0, 0.5)), -1.0, 1.0)
     return nothing
 end
 
 # Implementation of the periodic callback that uses the affect function above to update the value of ζ in threePlusOneDimensions every 0.01 t.
 cb = PeriodicCallback(affect!, 0.01)
 
-# Calculation of the trajectory based on the rules above
+# Function to reduce the number of timepoints in the data
+# Reduction factor is the kth element to keep (refer to proposal)
+function reduceTimepoints(data, reductionFactor)
+    # Return every reductionFactor-th element of the data, starting from the first element
+    return data[1:reductionFactor:end]
+end
+
+# Calculation of the trajectory based on the rules above and return it as a statespaceset
 function calculateTrajectory(p₀)
     u₀ = [0.0, 0.01, 0.0, 0.0]
-    prob = ODEProblem(threePlusOneDimensions!, u₀, (0.0,6000.0), p₀)
-    X = solve(prob, CVODE_BDF(), callback=cb, reltol = 1e-9, abstol = 1e-9)
-    return X
+    prob = ODEProblem(threePlusOneDimensions!, u₀, (0.0,12000.0), p₀)
+    data = solve(prob, Tsit5(), callback = cb, reltol = 1e-9, abstol = 1e-9, saveat = 0.01)
+    data = unique(DataFrame(data))
+    data = StateSpaceSet(data[:,2])
+
+    # Initially reduce the data set so that each points reflects one measurement per day
+    data = reduceTimepoints(data, 100)
+    return data
 end
 
-function keepX(data)
-    return [i[1] for i in data.u]
-end
-
-# Set the starting conditions and parameters of the model, and calculate the trajectory
+# Set the starting conditions and parameters of the model, and calculate the trajectory.
+# I strongly recommend only running one at a time, to avoid running out of memory
 healthy = calculateTrajectory([10, 1, 0.1, 14, 10, 1.04, 0.05, 0.2, 14, 4, 0.5, 0.5, 1, 0.2, 1, 720])
 schizophrenia = calculateTrajectory([10, 1, 0.1, 14, 10, 0.904, 0.05, 0.2, 14, 4, 0.5, 0.5, 1, 0.2, 1, 720])
 bipolar = calculateTrajectory([10, 1, 0.1, 14, 10, 1.04, 0.05, 1.01, 14, 10, 0.5, 0.5, 1, 0.2, 1, 720])
 bereavement = calculateTrajectory([10, 1, 0.1, 14, 10, 1, 0.05, 0.6, 14, 4.5, 0.5, 0.5, 1, 0.2, 1, 720])
 
-# Only save the x's from the model
-healthyX = keepX(healthy)
-schizophreniaX = keepX(schizophrenia)
-bipolarX = keepX(bipolar)
-bereavementX = keepX(bereavement)
+reduced = reduceTimepoints(healthy, 256)
 
+# Plot and calculate the recurrence statistics
+
+# Function to bin the data into a specified number of bins
 function binData(data, n)
-    # Calculate the bin size
+    # Calculate the bin size based on the range of the data and the number of bins
     bin_size = (maximum(data) - minimum(data)) / n
-    # Bin the data
-    data = [value - mod(value, bin_size) for value in data]
+    # Bin the data by rounding down the data value to the nearest bin edge
+    binned_data = floor.(data / bin_size) * bin_size
     # Return the binned data
-    return data
+    return binned_data
 end
 
-function reduceEachKthTimepoint(data, reductionFactor)
-    reduced = [data...]
-    keepat!(reduced, 1:reductionFactor:length(data))
+# Function to reduce the number of timepoints in the data
+function reduceTimepoints(data, reductionFactor)
+    # Return every reductionFactor-th element of the data, starting from the first element
+    return data[1:reductionFactor:end]
 end
 
+# Function to degrade the data by binning it and reducing the number of timepoints
 function degradeData(data, segmentN, reductionFactor)
+    # Bin the data into segmentN bins
     reduced = binData(data, segmentN)
-    # Remove unwanted timepoints and return
+    # Remove unwanted timepoints and return the reduced data
     return reduceTimepoints(reduced, reductionFactor)
 end   
- 
+
 binData(healthyX, 5)
+
+# Calculate the recurrence matrix for the data
+healthyRM = RecurrenceMatrix(healthy, 0.1)
+schizophreniaRM = RecurrenceMatrix(schizophrenia, 0.1) 
+bipolarRM = RecurrenceMatrix(bipolar, 0.1)
+bereavementRM = RecurrenceMatrix(bereavement, 0.1)
+
+# Do RQA
+healthyRQA = rqa(healthyRM)
+schizophreniaRQA = rqa(schizophreniaRM)
+bipolarRQA = rqa(bipolarRM)
+bereavementRQA = rqa(bereavementRM)
+
+
